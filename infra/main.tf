baa3ad832e07674147d930f07a80f1535a91e47a
zai-hub <zainabt.987@gmail.com>
2025-12-30 07:20:26 +0000

    working state of networking + security modules

---
+++ infra/main.tf
@@ -0,0 +1,15 @@
+# Root for Networking
+
+module "networking" {
+  source   = "./modules/networking"
+  vpc_cidr = var.vpc_cidr
+  az_count = var.az_count
+}
+
+# Root for Security
+
+module "security" {
+  source   = "./modules/security"
+  vpc_id   = module.networking.vpc_id
+  app_port = 80
+}
---
+++ infra/output.tf
@@ -0,0 +1,21 @@
+# Networking
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.networking.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.networking.private_subnet_ids
+}
+
+# Security
+output "alb_sg_id" { 
+  value = module.security.alb_sg_id
+  }
+
+output "ecs_tasks_sg_id" {
+  value = module.security.ecs_tasks_sg_id
+  }
